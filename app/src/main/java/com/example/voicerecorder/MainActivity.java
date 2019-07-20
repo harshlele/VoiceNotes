a546dac51ba4cfb004ab792e714570a1f36b03e9
Harshal Lele <harshallele97@gmail.com>
2019-07-20 22:21:38 +0530

    added FAB

--- app/src/main/java/com/example/voicerecorder/MainActivity.java
+++ app/src/main/java/com/example/voicerecorder/MainActivity.java
@@ -3,12 +3,29 @@ package com.example.voicerecorder;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FloatingActionButton newRecordingBtn;
+
+    private static final String TAG = "recorder";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        newRecordingBtn = findViewById(R.id.new_recording_fab);
+        newRecordingBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: clicked!");
+            }
+        });
+
     }
 }
