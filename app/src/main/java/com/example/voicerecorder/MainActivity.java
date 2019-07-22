4aa3c90df3ba7782df64ebbaabd7bb1b50c9ab01
Harshal Lele <harshallele97@gmail.com>
2019-07-22 12:17:50 +0530

    added FAB icons

--- app/src/main/java/com/example/voicerecorder/MainActivity.java
+++ app/src/main/java/com/example/voicerecorder/MainActivity.java
@@ -14,17 +14,18 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "recorder";
 
+    private boolean isRecording = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         newRecordingBtn = findViewById(R.id.new_recording_fab);
-        newRecordingBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d(TAG, "onClick: clicked!");
-            }
+        newRecordingBtn.setOnClickListener(view -> {
+            isRecording = !isRecording;
+            if(isRecording) newRecordingBtn.setImageDrawable(getDrawable(R.drawable.ic_mic_off_white_40dp));
+            else newRecordingBtn.setImageDrawable(getDrawable(R.drawable.ic_mic_white_40dp));
         });
 
     }
